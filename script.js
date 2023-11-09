581b8801dc7a1d936c5a1b47c4bde354e01ccdb7
PaoloPascucci <pascuccip9@gmail.com>
2023-11-09 17:26:46 +0100

    Update search input

--- script.js
+++ script.js
@@ -5,69 +5,75 @@ const submitBtn = document.querySelector(".queryB");
 const loadB = document.querySelector(".loadImg");
 const loadBM = document.querySelector(".loadImg2");
 
-function fetchDataAndDisplayImages(query, loadButtonSelector) {
-  const loadButton = loadButtonSelector;
+let currentQuery = "Tigers";
 
-  loadButton.addEventListener("click", function () {
-    const url = `https://api.pexels.com/v1/search?query=${query}`;
+function fetchDataAndDisplayImages(query) {
+  const url = `https://api.pexels.com/v1/search?query=${query}`;
 
-    fetch(url, {
-      headers: {
-        Authorization: apiKey,
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        const imgToChange = document.querySelectorAll(".card-img-top");
-        const idToChange = document.querySelectorAll(".idN");
-        const titleToChange = document.querySelectorAll(".card-title");
-        console.log(data);
-        for (let i = 0; i < data.photos.length; i++) {
-          const cardImage = document.createElement("img");
-          cardImage.src = data.photos[i].src.original;
-          cardImage.classList.add("card-img-top", "imgLink");
-          cardImage.alt = data.photos[i].alt;
-          cardImage.setAttribute("style", "width:100%; height:225px; cursor:pointer");
+  fetch(url, {
+    headers: {
+      Authorization: apiKey,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const imgToChange = document.querySelectorAll(".card-img-top");
+      const idToChange = document.querySelectorAll(".idN");
+      const titleToChange = document.querySelectorAll(".card-title");
+      console.log(data);
+      for (let i = 0; i < data.photos.length; i++) {
+        const cardImage = document.createElement("img");
+        cardImage.src = data.photos[i].src.original;
+        cardImage.classList.add("card-img-top", "imgLink");
+        cardImage.alt = data.photos[i].alt;
+        cardImage.setAttribute("style", "width:100%; height:225px; cursor:pointer");
 
-          const viewButtons = document.querySelectorAll(".view, .imgLink");
+        const viewButtons = document.querySelectorAll(".view, .imgLink");
 
-          viewButtons.forEach((btn, i) => {
-            btn.addEventListener("click", function () {
-              window.location.href =
-                "./img-template.html?urlImg=" +
-                encodeURIComponent(data.photos[i].src.original) +
-                "&urlName=" +
-                encodeURIComponent(data.photos[i].alt) +
-                "&urlArt=" +
-                encodeURIComponent(data.photos[i].photographer) +
-                "&urlPL=" +
-                encodeURIComponent(data.photos[i].photographer_url);
-            });
+        viewButtons.forEach((btn, i) => {
+          btn.addEventListener("click", function () {
+            window.location.href =
+              "./img-template.html?urlImg=" +
+              encodeURIComponent(data.photos[i].src.original) +
+              "&urlName=" +
+              encodeURIComponent(data.photos[i].alt) +
+              "&urlArt=" +
+              encodeURIComponent(data.photos[i].photographer) +
+              "&urlPL=" +
+              encodeURIComponent(data.photos[i].photographer_url);
           });
+        });
 
-          if (titleToChange[i]) {
-            titleToChange[i].textContent = data.photos[i].alt;
-            titleToChange[i].setAttribute("style", "height:60px");
-          }
-          if (idToChange[i]) {
-            idToChange[i].textContent = data.photos[i].id;
-          }
-          if (imgToChange[i]) {
-            imgToChange[i].replaceWith(cardImage);
-          }
+        if (titleToChange[i]) {
+          titleToChange[i].textContent = data.photos[i].alt;
+          titleToChange[i].setAttribute("style", "height:60px");
         }
-      })
-      .catch((error) => console.log("Error: " + error));
-  });
+        if (idToChange[i]) {
+          idToChange[i].textContent = data.photos[i].id;
+        }
+        if (imgToChange[i]) {
+          imgToChange[i].replaceWith(cardImage);
+        }
+      }
+    })
+    .catch((error) => console.log("Error: " + error));
 }
 
 submitBtn.addEventListener("click", function () {
   const newQuery = document.querySelector("#queryI").value;
-  fetchDataAndDisplayImages(newQuery, loadB);
+  currentQuery = newQuery;
+  fetchDataAndDisplayImages(currentQuery);
 });
 
-fetchDataAndDisplayImages("Tigers", loadB);
-fetchDataAndDisplayImages("Dogs", loadBM);
+loadB.addEventListener("click", function () {
+  fetchDataAndDisplayImages("Tigers");
+  currentQuery = "Tigers";
+});
+
+loadBM.addEventListener("click", function () {
+  fetchDataAndDisplayImages("Dogs");
+  currentQuery = "Dogs";
+});
 
 const hideButtons = document.querySelectorAll(".hide");
 
